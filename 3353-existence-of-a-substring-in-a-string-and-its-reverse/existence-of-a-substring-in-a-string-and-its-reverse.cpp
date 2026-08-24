45dadf50e4208802ca8906087e06a3ae867cdc8c
Anandi <anandimahajan60@gmail.com>
2026-08-24 20:09:28 +0530

    Time: 3 ms (37.59%) | Memory: 9.9 MB (25.19%) - LeetSync

--- 3353-existence-of-a-substring-in-a-string-and-its-reverse/existence-of-a-substring-in-a-string-and-its-reverse.cpp
+++ 3353-existence-of-a-substring-in-a-string-and-its-reverse/existence-of-a-substring-in-a-string-and-its-reverse.cpp
@@ -1,27 +1,35 @@
 class Solution {
 public:
     bool isSubstringPresent(string s) {
-        string snew = s ; 
-         reverse(s.begin() , s.end()) ; 
         int n = s.length() ; 
-        int windowLen = 2 ; 
-        for(int i=0 ; i<n ; i++) {
-          int windowIndx = 0 ;  
-          int idx = i ; 
-          string check = "" ;
-          while( idx<n && windowIndx < windowLen  ) {
-             check += snew[idx] ; 
-             idx++ ; 
-             windowIndx++ ;  
-          }
-          if(check.length() != 2) {
-            continue ; 
-          }
-          if(s.find(check) < n) {
-            return true ; 
-          }
+        unordered_set<string> s1  ; 
+        for(int i=0 ; i<n-1 ; i++) {
+           int idx = i ; 
+           int windsize = 0 ; 
+           string check = "" ;
+           while(idx<n && windsize<2 ) {
+              check += s[idx] ; 
+              idx++ ; 
+              windsize++ ; 
+           } 
+           s1.insert(check) ;
         }
 
+        reverse(s.begin() , s.end()) ; 
+        for(int i=0 ; i<n-1 ; i++) {
+            int idx = i ; 
+            int windsize = 0 ;
+            string check = "" ; 
+            while(idx<n && windsize<2) {
+                 check += s[idx] ; 
+                 idx++ ; 
+                 windsize++ ; 
+            }
+            if(s1.find(check) != s1.end()) {
+                return true ; 
+            } 
+        }
         return false ; 
+        
     }
 };
