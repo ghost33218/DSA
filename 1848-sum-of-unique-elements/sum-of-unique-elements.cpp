0547bc49fe8e9796b1c0335e58ae0c284cb88bb8
Anandi <anandimahajan60@gmail.com>
2026-08-29 13:12:27 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (94.03%) - LeetSync

--- 1848-sum-of-unique-elements/sum-of-unique-elements.cpp
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     int sumOfUnique(vector<int>& nums) {
-        int n = nums.size() ; 
-        int count = 0 ;
+            
+     sort(nums.begin() , nums.end()) ; 
+     int n = nums.size() ;
+     int count = 0 ;
+  
+     for(int i=0 ; i<n ; i++) {
+        int curr = nums[i] ; 
+        int flag = 0 ; //assuming it is unique 
+         i++ ;
+        while(i< n && curr==nums[i]) { 
+            flag = 1 ; 
+            i++ ; 
+        }
+        if(flag == 0) {
+            count += curr ;
+        }
+        i-- ; 
 
-        for(int  i=0 ; i<n ; i++) {
-            int flag = 0 ;  //assume the number is unique 
-            for(int j=0 ; j<n ; j++) {
-                if(nums[i] == nums[j] && i!=j) {
-                    flag = 1 ; 
-                    break ; 
-                }
-            } 
-            if(flag == 0) {
-                count += nums[i] ; 
-            }
-        }return count ; 
-    
+     }
+     return count ; 
     }
 };
