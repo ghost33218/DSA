6002270fe213f9094086c7d5b856f3930fd7eec5
Anandi <anandimahajan60@gmail.com>
2026-09-05 10:42:37 +0530

    Time: 7 ms (10.49%) | Memory: 9.8 MB (29.96%) - LeetSync

--- 3353-existence-of-a-substring-in-a-string-and-its-reverse/existence-of-a-substring-in-a-string-and-its-reverse.cpp
+++ 3353-existence-of-a-substring-in-a-string-and-its-reverse/existence-of-a-substring-in-a-string-and-its-reverse.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
     bool isSubstringPresent(string s) {
-        int n = s.length() ; 
-        unordered_set<string> s1  ; 
-        for(int i=0 ; i<n-1 ; i++) {
-           int idx = i ; 
-           int windsize = 0 ; 
-           string check = "" ;
-           while(idx<n && windsize<2 ) {
+        int n = s.length() ;  
+        unordered_set <string> s1 ; 
+
+        for(int i=0 ; i<n ; i++) {
+            int idx = i ; 
+            int windsize = 0 ;
+            string check = "" ;
+            while(idx<n && windsize<2) {
+                check += s[idx] ; 
+                idx++ ; 
+                windsize++ ; 
+
+            } 
+            if(check.length() == 2) {
+            s1.insert(check) ; }
+        }
+        reverse(s.begin() , s.end()) ; 
+
+        for(int i=0 ; i<n ; i++) {
+            int idx = i ; 
+            int windsize = 0 ; 
+            string check = "" ;
+            while(idx < n && windsize<2) {
               check += s[idx] ; 
               idx++ ; 
               windsize++ ; 
-           } 
-           s1.insert(check) ;
-        }
-
-        reverse(s.begin() , s.end()) ; 
-        for(int i=0 ; i<n-1 ; i++) {
-            int idx = i ; 
-            int windsize = 0 ;
-            string check = "" ; 
-            while(idx<n && windsize<2) {
-                 check += s[idx] ; 
-                 idx++ ; 
-                 windsize++ ; 
-            }
-            if(s1.find(check) != s1.end()) {
-                return true ; 
             } 
+            if(s1.find(check) != s1.end()) {
+                return true ;
+            }
         }
-        return false ; 
+        return false ;
         
     }
-};
+}; 
