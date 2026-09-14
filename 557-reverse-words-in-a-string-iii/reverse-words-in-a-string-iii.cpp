3a7e8eda7d2da6992520e5625036a9646775e7a7
Anandi <anandimahajan60@gmail.com>
2026-09-14 22:06:21 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (9.01%) - LeetSync

---
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+
+        string ans = "" ;
+        int n = s.length() ;
+
+        for(int i=0 ; i<n ; i++) {
+            string word = "" ;
+            while(i < n && s[i]!=' ') {
+                word += s[i] ;
+                i++ ;
+            }
+
+            if(word.length() != 0) {
+                reverse(word.begin() , word.end()) ;
+                ans += ' ' + word ; 
+            }
+        }
+
+        return ans.substr(1) ;
+    
+    }
+};
