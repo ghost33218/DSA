1ca8fd0c294e984f512a014efc1405934572678f
Anandi <anandimahajan60@gmail.com>
2026-09-17 22:56:06 +0530

    Time: 73 ms (49.79%) | Memory: 111.3 MB (46.35%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s ; 
+        int n  = nums.size() ; 
+
+        for(int i=0 ; i<n ; i++) {
+            if(s.find(nums[i]) == s.end()) {
+                s.insert(nums[i]) ;
+            }
+            else {
+                return true ;
+            }
+        }
+        return false ; 
+    }
+};
